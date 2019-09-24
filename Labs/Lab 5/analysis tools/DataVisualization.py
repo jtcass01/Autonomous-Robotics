e6dc76e3e5c9c063aef4e6888797f9e30f9c3af3
Jacob Cassady <jtcass01@louisville.edu>
2019-09-24 00:57:31 -0400

    Generated data visualizations for runs from today.

--- Labs/Lab 5/analysis tools/DataVisualization.py
+++ Labs/Lab 5/analysis tools/DataVisualization.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 
 class DataVisualization():
     @staticmethod
-    def plot_goal_and_actual(goal, actual_data, leftMotorPowers=None, rightMotorPowers=None):
-        assert (leftMotorPowers is None and rightMotorPowers is None) or (leftMotorPowers is not None and rightMotorPowers is not None)
+    def plot_goal_and_actual(goal, actual_data, left_motor_powers=None, right_motor_powers=None):
+        assert (left_motor_powers is None and right_motor_powers is None) or (left_motor_powers is not None and right_motor_powers is not None)
 
         time = np.zeros(actual_data.shape)
 
@@ -18,14 +18,14 @@ class DataVisualization():
         fig, axis = plt.subplots()
         axis.plot(time, goal, c='black', label="goal")
         axis.scatter(time, actual_data, s=10, marker='o', c='blue', label="actual_data")
-        axis.set_ylim((goal[0] - 300, goal[0] + 300))
+        axis.set_ylim((0, 4096))
         axis.set_ylabel("Sensor Response")
 
-        if leftMotorPowers is not None:
+        if left_motor_powers is not None:
             axis2 = axis.twinx()
-            axis2.scatter(time, leftMotorPowers, s=10, marker='s', c='green', label="Left Motor Power")
-            axis2.scatter(time, rightMotorPowers, s=10, marker='s', c='yellow', label="Right Motor Power")
-            axis2.set_ylim((-110, 110))
+            axis2.scatter(time, left_motor_powers, s=10, marker='s', c='green', label="Left Motor Power")
+            axis2.scatter(time, right_motor_powers, s=10, marker='s', c='yellow', label="Right Motor Power")
+            axis2.set_ylim((-105, 105))
             axis2.set_ylabel("Motor Power")
 
         plt.xlabel("Time (ms)")
@@ -74,4 +74,5 @@ if __name__ == "__main__":
             DataVisualization.plot_goal_and_actual(goal, actual_data)
         elif response == "2":
             goal, actual_data = input_goal_and_actual()
-            leftMotorPowers, rightMotorPowers = input_motor_powers()
+            left_motor_powers, right_motor_powers = input_motor_powers()
+            DataVisualization.plot_goal_and_actual(goal, actual_data, left_motor_powers=left_motor_powers, right_motor_powers=right_motor_powers)
