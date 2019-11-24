4126411c194e99a14e63ed44f2e0eef6baec022d
Jacob Cassady <jtcass01@louisville.edu>
2019-11-24 15:42:11 -0500

    Finished taking readings for field tests on laptop

--- Labs/Lab 6/analysis tools/DataVisualization.py
+++ Labs/Lab 6/analysis tools/DataVisualization.py
@@ -4,23 +4,23 @@ from sklearn.linear_model import LinearRegression
 
 if __name__ == "__main__":
 
-    distances = np.array(range(13,0,-1)).reshape((-1, 1))
-    analog_readings = np.array([97, 95, 88, 80, 75, 66, 57, 55, 55, 55, 55, 55, 55]).reshape((-1, 1))
+    distances = np.array(range(0,32,2)).reshape((-1, 1))
+    analog_readings = np.array([54, 54, 54, 54, 54, 54, 54, 54, 54, 64, 65, 68, 77, 88, 92, 100]).reshape((-1, 1))
 
-    non_dead_distances = distances[:7, 0].reshape((-1, 1))
-    non_dead_analog_readings = analog_readings[:7, 0].reshape((-1, 1))
+    non_dead_analog_readings = analog_readings[9:, 0].reshape((-1, 1))
+    non_dead_distances = distances[9:, 0].reshape((-1, 1))
 
     print(non_dead_distances, non_dead_analog_readings)
 
     regressor = LinearRegression()
     regressor.fit(non_dead_distances, non_dead_analog_readings)
 
-    print(regressor.intercept_)
-    print(regressor.coef_)
+    print("intercept:", regressor.intercept_)
+    print("coefficient",  regressor.coef_)
 
     plt.scatter(distances, analog_readings, c='blue', label="Sensor Response")
-    x = np.linspace(0,13, 2)
-    y = 6.82142857*x + 11.5
+    x = np.linspace(0,32, 2)
+    y = 3.25*x + 1.14285714
     plt.plot(x, y, c='black', label="Linear Fit")
     plt.xlabel('Inches')
     plt.ylabel('Analog Response')
